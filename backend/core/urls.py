038649b4fae172c6c1ba5e82825709ad5c3d9b2c
Lucas-Blanger <blangerlucas@gmail.com>
2026-02-05 14:02:41 -0300

    chore: configurate the project

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from dotenv import load_dotenv
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
